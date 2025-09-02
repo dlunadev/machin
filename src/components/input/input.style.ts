fa91892a0f93c899aa72072e1e77362e82adc30a
David <dluna.dev@gmail.com>
2025-09-02 01:48:23 -0300

    feat: implement Input component with customizable props and styles

---
+++ src/components/input/input.style.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  input: {
+    height: 48,
+    minHeight: 44,
+    borderWidth: 1,
+    borderRadius: 10,
+    flexDirection: "row",
+    paddingHorizontal: 16,
+    marginTop: 8,
+  },
+  slot: { 
+    alignItems: 'center',
+    justifyContent: 'center', 
+    height: 'auto' }
+});
+
+export default styles;
