33360e81b732f8448176c04d77985f9546e7887f
David <dluna.dev@gmail.com>
2025-09-02 01:49:11 -0300

    feat: implement NewPassword component with form inputs and submission handling

---
+++ src/app/(auth)/password/new-password/index.tsx
@@ -0,0 +1,115 @@
+import { Machin } from "@/assets/svg";
+import { ArrowLeftIcon, Button, Container, HStack, Icon, Input, KeyboardContainer } from "@/src/components";
+import { Text } from "@/src/components/text/text.component";
+import { Center } from "@/src/components/ui/center";
+import { Colors } from "@/src/constants/Colors";
+import { useCustomToast } from "@/src/hooks/utils/useToast";
+import { new_password_schema } from "@/src/utils/schemas/auth/new-password";
+import { useRouter } from "expo-router";
+import { Formik } from "formik";
+import React, { useState } from "react";
+import { Pressable, View } from "react-native";
+
+function NewPassword() {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  const { showToast } = useCustomToast();
+
+  const fakeRequest = (values: { password: string; new_password: string }) => {
+    setLoading(true);
+
+    setTimeout(() => {
+      setLoading(false);
+      showToast({
+        title: "Inicio de sesión",
+        children: `Correo: ${values.password}\nContraseña: ${values.new_password}`,
+      });
+    }, 500);
+  };
+
+  return (
+    <Container>
+      <KeyboardContainer>
+        <Center className="flex flex-1">
+          <View className="gap-7 items-center mb-14">
+            <Machin />
+            <Text size={20} color={Colors.TEXT} weight={600}>
+              Establecer{" "}
+              <Text size={20} color={Colors.PRIMARY} weight={600}>
+                nueva contraseña
+              </Text>
+            </Text>
+          </View>
+          <Formik
+            initialValues={{ password: "", new_password: "" }}
+            validationSchema={new_password_schema}
+            onSubmit={(values) => fakeRequest(values)}
+          >
+            {({
+              handleChange,
+              handleBlur,
+              handleSubmit,
+              values,
+              errors,
+              touched,
+            }) => (
+              <View className="w-full gap-7">
+                <View className="gap-4">
+                  <Input
+                    label="Nueva Contraseña"
+                    placeholder="Escribe tu contraseña aquí..."
+                    secureTextEntry
+                    rightIcon
+                    onChangeText={handleChange("password")}
+                    onBlur={handleBlur("password")}
+                    value={values.password}
+                    touched={touched.password}
+                    error={
+                      touched.password && errors.password
+                        ? errors.password
+                        : undefined
+                    }
+                  />
+                  <Input
+                    label="Confirmar contraseña"
+                    placeholder="Escribe tu contraseña aquí..."
+                    secureTextEntry
+                    rightIcon
+                    onChangeText={handleChange("new_password")}
+                    onBlur={handleBlur("new_password")}
+                    value={values.new_password}
+                    touched={touched.new_password}
+                    error={
+                      touched.new_password && errors.new_password
+                        ? errors.new_password
+                        : undefined
+                    }
+                  />
+                </View>
+
+                <Button onPress={() => handleSubmit()} loading={loading}>
+                  Ingresar
+                </Button>
+              </View>
+            )}
+          </Formik>
+          <Pressable
+            onPress={() => router.back()}
+            className="gap-2 items-center justify-center mt-5"
+          >
+            <HStack className="gap-2 items-center justify-center">
+              <Icon
+                as={ArrowLeftIcon}
+                size="lg"
+                style={{ color: Colors.BLACK }}
+              />
+              <Text size={14}>Volver a Iniciar sesión</Text>
+            </HStack>
+          </Pressable>
+        </Center>
+      </KeyboardContainer>
+    </Container>
+  );
+}
+
+export default NewPassword;
