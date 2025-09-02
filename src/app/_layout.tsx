3424f83a3f34feb9e6d551cca0b699d3f6b362d1
David <dluna.dev@gmail.com>
2025-09-02 01:49:25 -0300

    feat: update layout components for improved navigation and styling

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,19 +1,17 @@
-import { useColorScheme } from 'react-native';
-import '../../global.css';
 import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect } from 'react';
-import { Stack } from 'expo-router';
-import { GluestackUIProvider } from '../components/ui/gluestack-ui-provider';
 import 'react-native-reanimated';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import '../../global.css';
+import { GluestackUIProvider } from '../components/ui/gluestack-ui-provider';
 
 
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../../assets/fonts/SpaceMono-Regular.ttf'),
   });
@@ -35,7 +33,7 @@ export default function RootLayout() {
           <Stack.Screen name="(auth)" options={{ headerShown: false }} />
           <Stack.Screen name="+not-found" />
         </Stack>
-        <StatusBar style="auto" />
+        <StatusBar style='dark' />
       </SafeAreaProvider>
     </GluestackUIProvider>
   );
