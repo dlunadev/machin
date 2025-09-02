19b3e06469f51695e35ee5153e429150fe0db11f
David <dluna.dev@gmail.com>
2025-09-02 01:48:29 -0300

    fix: adjust padding in Container component for improved layout

--- src/components/container/container.component.tsx
+++ src/components/container/container.component.tsx
@@ -9,7 +9,7 @@ export const Container = ({ children }: { children: ReactNode }) => {
 
   return (
     <View
-      className={`flex flex-1 px-9`}
+      className={`flex flex-1 px-8`}
       style={[{ paddingTop: insets.top }, styles.container]}
     >
       {children}
