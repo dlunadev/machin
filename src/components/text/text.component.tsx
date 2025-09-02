7a819d0f09c62e76dd43de8e5928c64a72e5085f
David <dluna.dev@gmail.com>
2025-09-02 01:48:05 -0300

    feat: refactor Text component props for consistency and clarity

--- src/components/text/text.component.tsx
+++ src/components/text/text.component.tsx
@@ -1,18 +1,18 @@
-import React, { useState } from 'react';
-import { Text as RNText, TextProps as RNTextProps } from 'react-native';
-import { Colors } from '../../constants/Colors';
-import { scaleSize } from '../../helpers/scale-size';
-import styles from './styles';
-import { TextProps } from './text.type';
+import React, { useState } from "react";
+import { Text as RNText, TextProps as RNTextProps } from "react-native";
+import { Colors } from "../../constants/Colors";
+import { scaleSize } from "../../helpers/scale-size";
+import styles from "./styles";
+import { TextProps } from "./text.type";
 
 export const Text = (props: RNTextProps & TextProps) => {
   const {
     underline = false,
-    fontSize = 14,
+    size = 14,
     color = Colors.BLACK,
-    textAlign,
-    fontWeight = 300,
-    transform = 'none',
+    align,
+    weight = 300,
+    transform = "none",
     style,
     onPress,
     children,
@@ -22,7 +22,7 @@ export const Text = (props: RNTextProps & TextProps) => {
 
   const [isExpanded, setIsExpanded] = useState(false);
 
-  const textDecoration = underline ? 'underline' : 'none';
+  const textDecoration = underline ? "underline" : "none";
 
   let displayedText = children as string;
   if (maxLength && !isExpanded && displayedText.length > maxLength) {
@@ -33,18 +33,6 @@ export const Text = (props: RNTextProps & TextProps) => {
     setIsExpanded(!isExpanded);
   };
 
-  const fontWeightMap: { [key: number]: string } = {
-    100: 'Outfit-Thin',
-    200: 'Outfit-ExtraLight',
-    300: 'Outfit-Light',
-    400: 'Outfit-Regular',
-    500: 'Outfit-Medium',
-    600: 'Outfit-SemiBold',
-    700: 'Outfit-Bold',
-    800: 'Outfit-ExtraBold',
-    900: 'Outfit-Black',
-  };
-
   return (
     <RNText
       onPress={onPress}
@@ -53,10 +41,10 @@ export const Text = (props: RNTextProps & TextProps) => {
         {
           color,
           textDecorationLine: textDecoration,
-          fontSize: scaleSize(fontSize),
-          textAlign,
+          fontSize: scaleSize(size),
+          textAlign: align,
           textTransform: transform,
-          fontFamily: fontWeightMap[fontWeight],
+          fontWeight: weight,
         },
         style,
       ]}
--- src/components/text/text.type.ts
+++ src/components/text/text.type.ts
@@ -4,9 +4,9 @@ export type TextProps = {
   disabled?: boolean;
   color?: string;
   underline?: boolean;
-  fontSize?: number;
-  fontWeight?: number;
-  textAlign?: 'auto' | 'left' | 'right' | 'center' | 'justify' | undefined;
+  size?: number;
+  weight?: TextStyle['fontWeight'];
+  align?: 'auto' | 'left' | 'right' | 'center' | 'justify' | undefined;
   transform?: 'none' | 'capitalize' | 'uppercase' | 'lowercase';
   style?: TextStyle | TextStyle[];
   maxLength?: number;
