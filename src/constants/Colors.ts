a0026431c41e6b32e079092f347433f6037cee0c
David <dluna.dev@gmail.com>
2025-09-02 01:47:08 -0300

    feat: update PRIMARY color and ensure consistent formatting in Colors constant

--- src/constants/Colors.ts
+++ src/constants/Colors.ts
@@ -1,9 +1,11 @@
 export const Colors = {
-  PRIMARY: '#D90718',
+  PRIMARY: '#F23D4C',
   SECONDARY: '#2EC4B6',
   WHITE: '#F2F2F2',
+  TERTIARY: '#62748E',
   BLACK: '#000000',
   TEXT: '#0D0D0D',
   PINK: '#F7BBBE',
-  LIGHT_PINK: '#F29199'
+  LIGHT_PINK: '#F29199',
+  GRAY: '#B0B0B0',
 };
