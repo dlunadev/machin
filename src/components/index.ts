d7202d948e79ffbde1550aaa2309ef504977b329
David <dluna.dev@gmail.com>
2025-09-02 01:48:45 -0300

    feat: create index file to export components for easier imports

---
+++ src/components/index.ts
@@ -0,0 +1,15 @@
+export * from './button/button.component';
+export * from './container/container.component';
+export * from './input/input.component';
+export * from './input/input.interface';
+export * from './keyboard/keyboard.component';
+export * from './text/text.component';
+export * from './ui/center/index';
+export * from './ui/form-control/index';
+export * from './ui/hstack/index';
+export * from './ui/icon/index';
+export * from './ui/IconSymbol';
+export * from './ui/TabBarBackground';
+export * from './ui/toast/index';
+export * from './ui/vstack/index';
+
