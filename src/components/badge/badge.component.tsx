241c5c6bca99222842d1b47f197371e784233f00
David <dluna.dev@gmail.com>
2025-09-03 17:48:10 -0300

    feat: add Badge component with customizable variants and color schemes

---
+++ src/components/badge/badge.component.tsx
@@ -0,0 +1,34 @@
+import { Colors } from '@/src/constants/Colors';
+import { View } from 'react-native';
+import { Text } from '../text/text.component';
+
+type BadgeVariant = 'solid' | 'outline' | 'subtle';
+type BadgeAction = 'warning' | 'success' | 'info';
+
+type BadgeProps = {
+  label: string;
+  variant?: BadgeVariant;
+  colorScheme?: BadgeAction;
+  icon?: React.ReactNode;
+  className?: string;
+};
+
+const color_scheme: Record<BadgeAction, string> = {
+  success: Colors.LIGHT_GREEN,
+  warning: Colors.LIGHT_YELLOW,
+  info: Colors.LIGHT_BLUE,
+};
+
+export function Badge({ label, colorScheme, icon, className }: BadgeProps) {
+  return (
+    <View
+      className={`flex-row items-center gap-1 p-2 rounded-xl px-4 ${className ?? ''}`}
+      style={{
+        backgroundColor: color_scheme[colorScheme as BadgeAction],
+      }}
+    >
+      {icon}
+      <Text size={14} weight={400}>{label}</Text>
+    </View>
+  );
+}
