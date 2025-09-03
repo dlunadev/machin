aed52efd3f966182f67bd2821f445d8626461eba
David <dluna.dev@gmail.com>
2025-09-03 17:50:39 -0300

    feat: create turn actions component and types

---
+++ src/components/turn/turn-actions/turn-actions.component.tsx
@@ -0,0 +1,16 @@
+import { Button } from "../../button/button.component";
+import { HStack } from "../../ui/hstack";
+import { TurnActionsProps } from "./turn-actions.type";
+
+export const TurnActions = ({ primaryAction, secondaryAction }: TurnActionsProps) => {
+  return (
+    <HStack className="gap-2">
+      <Button onPress={primaryAction.onPress} outlined={primaryAction.outlined} icon={primaryAction.icon} left_icon stretch>
+        {primaryAction.label}
+      </Button>
+      <Button onPress={secondaryAction.onPress} icon={secondaryAction.icon} left_icon stretch>
+        {secondaryAction.label}
+      </Button>
+    </HStack>
+  );
+}
