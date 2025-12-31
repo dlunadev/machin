a4d0733567e9829c10722a684c9fcea9179d1ab4
David <dluna.dev@gmail.com>
2025-12-31 11:52:25 -0300

    fix(client): update use case with correct types

--- sdk/application/client/client.use-case.ts
+++ sdk/application/client/client.use-case.ts
@@ -1,22 +1,26 @@
 import { Client } from "@/sdk/domain/client/client.entity";
 import { ClientRepository } from "@/sdk/domain/client/client.repository";
-import { Pagination } from "@supabase/supabase-js";
+import { Pagination } from "@/sdk/utils/type/pagination";
 
 export class ClientUseCase implements ClientRepository {
-  private constructor(private repository: ClientRepository) { }
+  constructor(private repository: ClientRepository) { }
+  
   create(data: Client): Promise<Client> {
     return this.repository.create(data);
   }
-  find_all(data: Pagination): Promise<Client[]> {
+  
+  find_all(data: Pagination & { zone_id?: string }): Promise<Client[]> {
     return this.repository.find_all(data);
   }
+  
   find_by_id(id: string): Promise<Client> {
     return this.repository.find_by_id(id);
   }
+  
   update(id: string, data: Partial<Client>): Promise<Client> {
     return this.repository.update(id, data);
-
   }
+  
   delete(id: string): Promise<void> {
     return this.repository.delete(id);
   }
