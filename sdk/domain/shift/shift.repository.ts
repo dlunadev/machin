397e50b9ea39792fe282b6ce418fe0f8736e3b2c
David <dluna.dev@gmail.com>
2025-12-31 11:52:25 -0300

    fix(shift): update repository return type
    Change get_active_shifts to return Shift | null

--- sdk/domain/shift/shift.repository.ts
+++ sdk/domain/shift/shift.repository.ts
@@ -2,5 +2,5 @@ import { Repository } from "@/sdk/utils/interface/repository";
 import { Shift } from "./shift.entity";
 
 export interface ShiftRepository extends Repository<Shift> {
-  get_active_shifts(user_id: string): Promise<Shift>;
-};
+  get_active_shifts(user_id: string): Promise<Shift | null>;
+}
