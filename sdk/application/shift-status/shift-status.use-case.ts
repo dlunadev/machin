21429964e2772036ddc1dbf7a347048c121731b7
David <dluna.dev@gmail.com>
2025-12-31 11:52:26 -0300

    fix(shift-status): fix entity types in use case

--- sdk/application/shift-status/shift-status.use-case.ts
+++ sdk/application/shift-status/shift-status.use-case.ts
@@ -1,23 +1,23 @@
 import { ShiftStatusRepository } from "@/sdk/domain/shift-status/shift-status-repository";
-import { ShiftStatus } from "@/sdk/domain/shift-status/shift-status.entity";
-import { Pagination } from "@supabase/supabase-js";
+import { ShiftStatusEntity } from "@/sdk/domain/shift-status/shift-status.entity";
+import { Pagination } from "@/sdk/utils/type/pagination";
 
 export class ShiftStatusUseCase implements ShiftStatusRepository {
   constructor(private readonly repository: ShiftStatusRepository) { }
 
-  async create(data: ShiftStatus): Promise<ShiftStatus> {
+  async create(data: ShiftStatusEntity): Promise<ShiftStatusEntity> {
     return this.repository.create(data);
   }
 
-  async find_all(data: Pagination): Promise<ShiftStatus[]> {
+  async find_all(data: Pagination): Promise<ShiftStatusEntity[]> {
     return this.repository.find_all(data);
   }
 
-  async find_by_id(id: string): Promise<ShiftStatus> {
+  async find_by_id(id: string): Promise<ShiftStatusEntity> {
     return this.repository.find_by_id(id);
   }
 
-  async update(id: string, data: ShiftStatus): Promise<ShiftStatus> {
+  async update(id: string, data: Partial<ShiftStatusEntity>): Promise<ShiftStatusEntity> {
     return this.repository.update(id, data);
   }
 
