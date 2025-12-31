2261aa885b4932019bd853d839ae9785f8330827
David <dluna.dev@gmail.com>
2025-12-31 11:52:27 -0300

    fix(shift-clients): add missing create_clients_shift method

--- sdk/application/shift-clients/shift-clients.use-case.ts
+++ sdk/application/shift-clients/shift-clients.use-case.ts
@@ -4,18 +4,26 @@ import { Pagination } from "@/sdk/utils/type/pagination";
 
 export class ShiftClientsUseCase implements ShiftClientRepository {
   constructor(private readonly repository: ShiftClientRepository) { }
+  
   create(data: ShiftClients): Promise<ShiftClients> {
     return this.repository.create(data);
   }
+  
+  create_clients_shift(shift_id: string, payload: ShiftClients[]): Promise<ShiftClients[]> {
+    return this.repository.create_clients_shift(shift_id, payload);
+  }
+  
   find_all(data: Pagination): Promise<ShiftClients[]> {
     return this.repository.find_all(data);
   }
+  
   find_by_id(id: string): Promise<ShiftClients> {
     return this.repository.find_by_id(id);
   }
+  
   update(id: string, data: Partial<ShiftClients>): Promise<ShiftClients> {
     return this.repository.update(id, data);
-  };
+  }
 
   delete(id: string): Promise<void> {
     return this.repository.delete(id);
