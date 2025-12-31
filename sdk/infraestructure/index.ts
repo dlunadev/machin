eb943b563a2f616c4d74c87366f94a43f1ad2299
David <dluna.dev@gmail.com>
2025-12-31 11:52:27 -0300

    feat(infrastructure): export all mock adapters and configs

--- sdk/infraestructure/index.ts
+++ sdk/infraestructure/index.ts
@@ -1,1 +1,24 @@
+// Auth
+export * from './auth/auth.mock';
 export * from './auth/auth.supabase';
+
+// Client
+export * from './client/client.mock';
+export * from './client/client.supabase';
+
+// Zone
+export * from './zone/zone.mock';
+export * from './zone/zone.supabase';
+
+// Shift
+export * from './shift/shift.mock';
+export * from './shift/shift.supabase';
+
+// Shift Status
+export * from './shift-status/shift-status.mock';
+export * from './shift-status/shift-status.supabase';
+
+// Shift Clients
+export * from './clients-shift/clients-shift.mock';
+export * from './clients-shift/clients-shift.supabase';
+
