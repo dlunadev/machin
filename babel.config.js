c002cb01fad1a6e69c9c9b0eebbb216f0beeaf92
David <dluna.dev@gmail.com>
2026-02-16 04:24:31 -0300

    app:  things

--- babel.config.js
+++ babel.config.js
@@ -24,7 +24,7 @@ module.exports = function (api) {
           },
         },
       ],
-      'react-native-worklets-core/plugin',
+      // 'react-native-worklets-core/plugin',
       'react-native-reanimated/plugin',
     ],
   };
